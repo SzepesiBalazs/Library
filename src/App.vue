ff7b08be33599f6079c11af093a843b0d2b6c97f
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2024-10-13 16:23:03 +0200

    Added search option for books

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,33 @@ import userData from "./assets/libraryData.json";
 export default {
   setup() {
     const libraryData = ref(userData);
+    const searchParameter = ref("");
+    const filteredLibraryData = ref([]);
     const newBook = ref({
       title: "",
       author: "",
       year: "",
     });
+
+    const searchBook = () => {
+      filteredLibraryData.value = libraryData.value.books.filter(
+        (book) => book.title === searchParameter.value
+      );
+    };
     const addBook = () => {
       libraryData.value.books.push({ ...newBook.value });
       newBook.value.title = "";
       newBook.value.author = "";
       newBook.value.year = "";
     };
+
     return {
       libraryData,
       addBook,
       newBook,
+      searchParameter,
+      searchBook,
+      filteredLibraryData,
     };
   },
 };
@@ -36,16 +48,28 @@ export default {
     <div id="app"></div>
     <div class="wrapper">
       <div>
-        <ul>
-          <li v-for="book in libraryData.books" :key="book.id">
-            <p>{{ book.title }}</p>
-            <span>{{ book.author }}</span>
-          </li>
-        </ul>
-        <input v-model="newBook.title" />
-        <input v-model="newBook.author" />
-        <VueDatePicker v-model="newBook.year" year-picker />
-        <button type="submit" @click="addBook">Save</button>
+        <input v-model="searchParameter" />
+        <button type="submit" @click="searchBook">Search</button>
+        <div v-if="filteredLibraryData.length > 0">
+          <ul>
+            <li v-for="book in filteredLibraryData" :key="book.id">
+              <p>{{ book.title }}</p>
+              <span>{{ book.author }}</span>
+            </li>
+          </ul>
+        </div>
+        <div v-else>
+          <ul>
+            <li v-for="book in libraryData.books" :key="book.id">
+              <p>{{ book.title }}</p>
+              <span>{{ book.author }}</span>
+            </li>
+          </ul>
+          <input v-model="newBook.title" />
+          <input v-model="newBook.author" />
+          <VueDatePicker v-model="newBook.year" year-picker />
+          <button type="submit" @click="addBook">Save</button>
+        </div>
       </div>
 
       <nav>
