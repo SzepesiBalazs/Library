91ab0ecff8cb9cd8dd7587cdb71966bcf00238ee
SzepesiBalazs <szepesi.balazs0924@gmail.com>
2024-10-16 12:41:18 +0200

    Added Books component for better readibility

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,6 @@
-<script>
-import { ref } from "vue";
-import userData from "./assets/libraryData.json";
-import Books from "./components/Books.vue";
-import Books from "./components/Books.vue";
-export default {
-  setup() {
-    const libraryData = ref(userData);
-    const searchParameter = ref("");
-    const filteredLibraryData = ref([]);
-    const newBook = ref({
-      title: "",
-      author: "",
-      year: "",
-    });
-
-    const searchBook = () => {
-      filteredLibraryData.value = libraryData.value.books.filter(
-        (book) => book.title === searchParameter.value
-      );
-    };
-    const addBook = () => {
-      libraryData.value.books.push({ ...newBook.value });
-      newBook.value.title = "";
-      newBook.value.author = "";
-      newBook.value.year = "";
-    };
-
-    return {
-      libraryData,
-      addBook,
-      newBook,
-      searchParameter,
-      searchBook,
-      filteredLibraryData,
-    };
-  },
-};
-
-function deleteBooks(title) {
-  libraryData.value.books.splice(title, 1);
-}
-</script>
+<script></script>
 
 <template>
-  <Books
-    v-for="(item, index) in Books"
-    :title="item[0].title"
-    :author="item[0].author"
-    :year="item[0].year"
-    v-on:deleteBook="deleteBooks"
-  />
   <header>
     <img
       alt="Vue logo"
@@ -60,31 +11,6 @@ function deleteBooks(title) {
     />
     <div id="app"></div>
     <div class="wrapper">
-      <div>
-        <input v-model="searchParameter" />
-        <button type="submit" @click="searchBook">Search</button>
-        <div v-if="filteredLibraryData.length > 0">
-          <ul>
-            <li v-for="book in filteredLibraryData" :key="book.id">
-              <p>{{ book.title }}</p>
-              <span>{{ book.author }}</span>
-            </li>
-          </ul>
-        </div>
-        <div v-else>
-          <ul>
-            <li v-for="book in libraryData.books" :key="book.id">
-              <p>{{ book.title }}</p>
-              <span>{{ book.author }}</span>
-            </li>
-          </ul>
-          <input v-model="newBook.title" />
-          <input v-model="newBook.author" />
-          <VueDatePicker v-model="newBook.year" year-picker />
-          <button type="submit" @click="addBook">Save</button>
-        </div>
-      </div>
-
       <nav>
         <RouterLink to="/">Books</RouterLink>
         <RouterLink to="/about">About</RouterLink>
